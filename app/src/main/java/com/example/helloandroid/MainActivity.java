286e96ed16c90a9f098539c2bf7597181936668b
swseo <swseokitec@gmail.com>
2022-07-15 11:28:46 +0900

    slave branch 1st update

--- app/src/main/java/com/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Log.i("tag", "hello android");
+        Log.i("tag", "hello korea");
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
